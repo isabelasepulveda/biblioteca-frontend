0aceb6d485a8625fca242d4d4e6d06e14f42df22
isabelasepulveda <isabela_sepulveda82242@elpoli.edu.co>
2026-06-02 23:14:14 -0500

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://biblioteca-backend-yz1f.onrender.com',
+baseURL: 'https://biblioteca-backend-yz1f.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
